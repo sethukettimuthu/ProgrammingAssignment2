f98378c0754957e32e13ebd18557b01b1ccf1f53
Sethu Kettimuthu <sethu.kettimuthu@gmail.com>
2015-06-21 17:51:53 -0400

    Adding CacheMatrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function caches the matrix passed as an input parameter and exposes functions to get and set the input matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        #matrix m is set to null
+        m <- NULL
+        
+        #setter 
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        
+        #getter
+        get <- function() x
+        
+        #set a matrix
+        setmatrix <- function(matrix) m <<- matrix
+        
+        #get a matrix
+        getmatrix <- function() m
+        
+        #makeCachematrix function is a list of the following four functions 
+        list(set = set, get = get,setmatrix = setmatrix,getmatrix = getmatrix)
 }
 
 
-## Write a short comment describing this function
-
+## This function Computes the Inverse of the matrix if the inverse is not cached
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
+        m <- x$getmatrix()
+        #Checks if the matrix is cached and if the cache is not null it returns the cached inverse
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        
+        #matrix inverse function
+        m <- solve(data, ...)
+        x$setmatrix(m)
+        m
 }
